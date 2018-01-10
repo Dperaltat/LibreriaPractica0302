d8899f06ff2a1d991dd59034ba7d176a01db9d5f
pedro <pedro@172.16.221.100>
2018-01-10 08:39:39 -0500

    Lista Hecha

--- src/Lista/Lista.java
+++ src/Lista/Lista.java
@@ -1,7 +1,33 @@
 package Lista;
 
-public class Lista {
+import java.util.List;
 
+public class Lista<E> {
+
+    private List<E> elementoList;
+
+    public Lista(List<E> elementoList) {
+        this.elementoList = elementoList;
+    }
+
+    public List<E> getElementoList() {
+        return elementoList;
+    }
+
+    public void setElementoList(List<E> elementoList) {
+        this.elementoList = elementoList;
+    }
     
+    public boolean agregarElemento(E valor) {
+        return this.elementoList.add(valor);
+    }
+    
+    public E recuperarElemento(int id) {
+        return this.elementoList.get(id);
+    }
+    
+    public boolean eliminarElemento(int id) {
+        return this.elementoList.remove(this.getElementoList().get(id));
+    }
     
 }
